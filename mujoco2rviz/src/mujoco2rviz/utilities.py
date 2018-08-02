467467eaa0647540fbc6f458cf5a24d39eb34dcf
mikramarc <michal@shadowrobot.com>
2018-08-02 16:44:57 +0000

    fixing license

--- mujoco2rviz/src/mujoco2rviz/utilities.py
+++ mujoco2rviz/src/mujoco2rviz/utilities.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2018 Shadow Robot Company Ltd - All Rights Reserved.
+# Proprietary and Confidential. Unauthorized copying of the content in this file, via any medium is strictly prohibited.
 
 import re
 import os
