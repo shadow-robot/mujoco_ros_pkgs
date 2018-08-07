2d114d8fbb9db63d467e381a7ce30646ed8f990e
Giuseppe <giuseppe@shadowrobot.com>
2018-08-07 15:55:17 +0000

    repair script

--- mujoco_ros_utils/scripts/spawn_boxes_demo.py
+++ mujoco_ros_utils/scripts/spawn_boxes_demo.py
@@ -31,6 +31,20 @@ class ExampleSpawnSimClient(SpawnSimClient):
         rec_object_1.pose.pose.pose.orientation.z = 0
         rec_object_1.pose.pose.pose.orientation.w = 1
 
+        # Dummy obj 2
+        rec_object_2 = RecognizedObject()
+        rec_object_2.type.key = "box"
+        rec_object_2.pose.pose.pose.position.x = 0.3
+        rec_object_2.pose.pose.pose.position.y = 1
+        rec_object_2.pose.pose.pose.position.z = 0
+        rec_object_2.pose.pose.pose.orientation.x = 0.707
+        rec_object_2.pose.pose.pose.orientation.y = 0
+        rec_object_2.pose.pose.pose.orientation.z = 0
+        rec_object_2.pose.pose.pose.orientation.w = 0.707
+        request.objects = [rec_object_1, rec_object_2]
+        self._request_sim(request)
+
+
 if __name__ == '__main__':
     rospy.init_node("spawn_sim_client_example_node")
     spawn_sim = ExampleSpawnSimClient()
