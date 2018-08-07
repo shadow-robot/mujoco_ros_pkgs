01ebb4a3f95478de7bf0d1b603ec792b18384486
Giuseppe <giuseppe@shadowrobot.com>
2018-08-07 16:10:59 +0000

    fix lint

--- mujoco_ros_utils/src/mujoco_ros_utils/spawn_simulation.py
+++ mujoco_ros_utils/src/mujoco_ros_utils/spawn_simulation.py
@@ -61,7 +61,8 @@ class SpawnSimulation(object):
             rospy.loginfo("Starting simulation..")
             try:
                 process = subprocess.Popen(['xterm -e roslaunch fh_robot_launch fh_ur10_and_fh2_mujoco.launch sim:=true \
-                                            grasp_controller:=true scene:=true scene_file:="$(find mujoco_models)/mujoco.scene" robot_model_path:={}/{}'.format(self._xml_config_dir,
+                                            grasp_controller:=true scene:=true scene_file:="$(find mujoco_models)/mujoco.scene" \
+                                            robot_model_path:={}/{}'.format(self._xml_config_dir,
                                             self._generated_mujoco_env_filename)], shell=True)
             except OSError as e:
                 rospy.logerr("Could not spawn simulation")
