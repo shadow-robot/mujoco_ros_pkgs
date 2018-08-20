19a4ff6fb880893878ab64abc35d7b6b26f5c036
mikramarc <michal@shadowrobot.com>
2018-08-20 15:14:35 +0000

    changing to rostest

--- mujoco_ros_control/test/test_mujoco_ros_control.py
+++ mujoco_ros_control/test/test_mujoco_ros_control.py
@@ -3,15 +3,12 @@
 # Copyright (C) 2018 Shadow Robot Company Ltd - All Rights Reserved.
 # Proprietary and Confidential. Unauthorized copying of the content in this file, via any medium is strictly prohibited.
 
-# Use with: roslaunch mujoco_manual_tests multiple_type_objects.launch
-
-
-import argparse
 import rospy
+from unittest import TestCase
 from geometry_msgs.msg import Pose
 from mujoco_ros_msgs.msg import ModelStates
-from sr_utilities_common.manual_test_suite import ManualTestSuite
 
+PKG = "mujoco_ros_control"
 
 def list_to_pose(list):
     pose = Pose()
@@ -38,11 +35,10 @@ def compare_poses(pose_1, pose_2, epsilon_translation=0.1, epsilon_rotation=0.1)
     return poses_equal
 
 
-class MujocoRosControlTest(object):
-    def __init__(self):
+class TestMujocoRosControl(TestCase):
+    def setUp(self):
         self.published_objects = rospy.wait_for_message('/mujoco/model_states', ModelStates)
-        self.ignore_unknown_type_objects()
-
+        self._ignore_unknown_type_objects()
         self.objects_test_case = {'box_0': {'type': ModelStates.BOX,
                                             'is_static': True,
                                             'pose': [0, 0.7, 0.075, 0, 0, 0, 1]},
@@ -50,13 +46,13 @@ class MujocoRosControlTest(object):
                                                  'is_static': True,
                                                  'pose': [0.2, 0.7, 0.045, 0, 0, 0, 1]},
                                   'sphere_0': {'type': ModelStates.SPHERE,
-                                               'is_static': False,
-                                               'pose': [0.4, 0.7, 0.05, 0, 0, 0, 1]},
+                                                 'is_static': False,
+                                                 'pose': [0.4, 0.7, 0.05, 0, 0, 0, 1]},
                                   'box_1': {'type': ModelStates.MESH,
                                             'is_static': False,
                                             'pose': [0.6, 0.7, 0, 0, 0, 0.707, 0.707]}}
 
-    def ignore_unknown_type_objects(self):
+    def _ignore_unknown_type_objects(self):
         for idx, object_type in enumerate(self.published_objects.type):
             if 'unknown_type' == object_type:
                 del self.published_objects.name[idx]
@@ -65,48 +61,36 @@ class MujocoRosControlTest(object):
                 del self.published_objects.size[idx]
                 del self.published_objects.pose[idx]
 
+    def test_is_publishing(self):
+        is_publishing = False
+        try:
+            msg = rospy.wait_for_message('/mujoco/model_states', ModelStates)
+        except Exception:
+            pass
+        else:
+            is_publishing = True  
+        self.assertTrue(is_publishing)
+
     def test_objects_in_message(self):
-        result = True
         for object_name in self.objects_test_case:
-            if object_name not in self.published_objects.name:
-                result = False
-                break
-        return result
+            self.assertIn(object_name, self.published_objects.name)
 
     def test_object_types(self):
-        result = True
         for idx, object_type in enumerate(self.published_objects.type):
-            if object_type != self.objects_test_case[self.published_objects.name[idx]]['type']:
-                result = False
-                break
-        return result
+            self.assertEqual(object_type, self.objects_test_case[self.published_objects.name[idx]]['type'])
 
     def test_if_objects_static(self):
-        result = True
         for idx, is_static in enumerate(self.published_objects.is_static):
-            if is_static != self.objects_test_case[self.published_objects.name[idx]]['is_static']:
-                result = False
-                break
-        return result
+            self.assertEqual(is_static, self.objects_test_case[self.published_objects.name[idx]]['is_static'])
 
     def test_object_poses(self):
-        result = True
         for idx, pose in enumerate(self.published_objects.pose):
-            if not compare_poses(pose, list_to_pose(self.objects_test_case[self.published_objects.name[idx]]['pose'])):
-                print "pose of {} different!".format(self.published_objects.name[idx])
-                result = False
-                break
-        return result
+            self.assertTrue(compare_poses(pose, list_to_pose(self.objects_test_case[self.published_objects.name[idx]]['pose'])))
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--unattended", help="Run unattended (no user input).", action='store_true')
-    args, unknown_args = parser.parse_known_args()
-    rospy.init_node('mujoco_ros_control_test')
-    mujoco_ros_control_test = MujocoRosControlTest()
-    test_suite = ManualTestSuite(mujoco_ros_control_test, unattended=args.unattended)
-    test_suite.create_test('test_objects_in_message()')
-    test_suite.create_test('test_object_types()')
-    test_suite.create_test('test_if_objects_static()')
-    test_suite.create_test('test_object_poses()')
-    test_suite.print_summary()
+
+if __name__ == "__main__":
+    import rostest
+
+    rospy.init_node("test_mujoco_ros_control")
+    rostest.rosrun(PKG, "test_mujoco_ros_control", TestMujocoRosControl)
+
