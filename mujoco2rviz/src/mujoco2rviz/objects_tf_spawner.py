e5f69a95fe40feb20b8c81d0bba5b0ce960f8908
michal <michal@shadowrobot.com>
2018-11-20 15:53:53 +0000

    tfs for boxes working

--- mujoco2rviz/src/mujoco2rviz/objects_tf_spawner.py
+++ mujoco2rviz/src/mujoco2rviz/objects_tf_spawner.py
@@ -5,13 +5,15 @@
 
 import rospy
 import tf2_ros
+from math import sin, cos
 from mujoco_ros_msgs.msg import ModelStates
 from geometry_msgs.msg import TransformStamped
+from tf.transformations import euler_from_quaternion
 
 class Mujoco2RvizTfSpawner():
     def __init__(self):
         self.objects_to_poses_dict = {}
-        self.supported_types = ['mesh']
+        self.supported_types = ['mesh', 'box']
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
         rospy.Subscriber("/mujoco/model_states", ModelStates, self.model_states_cb)
 
@@ -37,11 +39,31 @@ class Mujoco2RvizTfSpawner():
 
         return tf
 
+    def move_pose_alongside_intrinsic_z_axis(self, pose, distance):
+        orientation_euler = euler_from_quaternion([pose.orientation.x,
+                                                          pose.orientation.y,
+                                                          pose.orientation.z,
+                                                          pose.orientation.w])
+        translation_normal_vector = [sin(orientation_euler[1]),
+                                     -sin(orientation_euler[0])*cos(orientation_euler[1]),
+                                     cos(orientation_euler[0])*cos(orientation_euler[1])]
+
+        translation_vector = [coord * distance for coord in translation_normal_vector]
+        pose.position.x += translation_vector[0]
+        pose.position.y += translation_vector[1]
+        pose.position.z += translation_vector[2]
+
+        return pose
+
     def process_data(self, data):
         object_types = data.type
         for idx, object_type in enumerate(object_types):
             if object_type in self.supported_types:
                 self.objects_to_poses_dict[data.name[idx]] = data.pose[idx]
+                if 'box' == object_type:
+                    # Move frame to the bottom of the object
+                    self.objects_to_poses_dict[data.name[idx]] = self.move_pose_alongside_intrinsic_z_axis(self.objects_to_poses_dict[data.name[idx]],
+                                                                                                                                      -data.size[idx].data[2])
 
 if __name__ == '__main__':
     rospy.init_node('mujoco_spawn_tfs')
