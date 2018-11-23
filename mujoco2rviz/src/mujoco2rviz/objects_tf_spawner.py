c29fc4042b21b08d009943d8a42ef513b22e7ce7
michal <michal@shadowrobot.com>
2018-11-23 12:17:08 +0000

    please

--- mujoco2rviz/src/mujoco2rviz/objects_tf_spawner.py
+++ mujoco2rviz/src/mujoco2rviz/objects_tf_spawner.py
@@ -66,8 +66,8 @@ class Mujoco2RvizTfSpawner():
                     object_name = data.name[idx]
                     object_height = data.size[idx].data[2] * 2
                     self.objects_to_poses_dict[data.name[idx]] = self.move_pose_alongside_intrinsic_z_axis(
-                                                                    self.objects_to_poses_dict[object_name],
-                                                                    -object_height/2)
+                                                                     self.objects_to_poses_dict[object_name],
+                                                                     -object_height/2)
 
 if __name__ == '__main__':
     rospy.init_node('mujoco_spawn_tfs')
